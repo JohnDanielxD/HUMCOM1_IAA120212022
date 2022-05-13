254a66a13762d76e995bb2bf76a1376a0c949192
JohnDanielxD <97924646+JohnDanielxD@users.noreply.github.com>
2022-05-13 20:23:59 +0800

    Add files via upload

---
+++ Chapter 15-17/Exercise15.js
@@ -0,0 +1,11 @@
+function displayAssignedValues(){
+    var firstName = document.getElementById("firstName").value = "John Daniel Perez";
+    var age = document.getElementById("age").value = "19";
+    var phoneNumber = document.getElementById("phoneNumber").value = "09564011374";
+    var eMail = document.getElementById("eMail").value = "20197519@s.ubaguio.edu";
+    var mailAddress = document.getElementById("mailAddress").value = "General Luna, Baguio City";
+    var program = document.getElementById("program").value = "Computer Science";
+    var idNum = document.getElementById("idNum").value = "20197519";
+    var yearLevel = document.getElementById("yearLevel").value = "1st Year";
+    var section = document.getElementById("section").value = "IAA1";
+}
